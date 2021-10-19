100d70943a2de125b58b6af31c3498f9fbbf4294
ruessanru <rssrsanru@gmail.com>
2021-10-18 20:18:02 -0500

    se realizo archivos tipos y autenticacionReducer

--- src/connection/autenticacionAcciones.js
+++ src/connection/autenticacionAcciones.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import axios from 'axios'
 import { LOGIN_ENDPOINT } from './helpers/endpoints'
 
-export const loginUsuario = (datosUsuario) => {
+export const loginUsuario = (datosUsuario) => dispatch => {
 
     console.log(datosUsuario)
 
--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension"
+import combineReducers from "./reducers/combineReducers";
 
 const middleware = [thunk]
 const store =createStore(
-
+    combineReducers,
+    composeWithDevTools(applyMiddleware(...middleware))
 )
 export {store}
